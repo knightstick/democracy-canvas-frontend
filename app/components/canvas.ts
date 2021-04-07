cd58b46018f96afcaaacb31ae2981992c6180491
Chris Jewell <chrisjohnjewell@gmail.com>
2021-04-07 16:28:26 +1000

    Add a button for drawing a random line

--- app/components/canvas.ts
+++ app/components/canvas.ts
@@ -1,14 +1,24 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
-interface CanvasArgs {
-}
+interface CanvasArgs {}
+// TODO
+// add a type for colour
+// add a type for a point
+// embiggen the canvas
+// use the canvas size for max random numbers
 
 const Colours = [
   'green',
   'purple',
-  'blue'
-]
+  'blue',
+  'aqua',
+  'teal',
+  'yellow',
+  'fuschia',
+  'orange',
+  'papyrus',
+];
 
 export default class Canvas extends Component<CanvasArgs> {
   canvasElement?: HTMLCanvasElement;
@@ -22,13 +32,42 @@ export default class Canvas extends Component<CanvasArgs> {
     const ctx = this.canvasElement?.getContext('2d')!;
 
     ctx.fillStyle = color;
-    ctx.fillRect(x, y, size, size)
+    ctx.fillRect(x, y, size, size);
   }
 
   @action
   drawRandomRectangle() {
-    const color = Colours[this.randomNumber(3)];
-    this.renderSquare(this.randomNumber(100),this.randomNumber(100), color,this.randomNumber(200))
+    const color = Colours[this.randomNumber(Colours.length)];
+    this.renderSquare(
+      this.randomNumber(100),
+      this.randomNumber(100),
+      color,
+      this.randomNumber(200)
+    );
+  }
+
+  drawLine(x1: number, y1: number, x2: number, y2: number, colour: any) {
+    const ctx = this.canvasElement?.getContext('2d')!;
+
+    ctx.beginPath();
+    ctx.strokeStyle = colour;
+    ctx.lineWidth = 3;
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+    ctx.closePath();
+  }
+
+  @action
+  drawRandomLine() {
+    const colour = Colours[this.randomNumber(Colours.length)];
+    this.drawLine(
+      this.randomNumber(100),
+      this.randomNumber(100),
+      this.randomNumber(100),
+      this.randomNumber(100),
+      colour
+    );
   }
 
   @action
