ad6f0061c729a3f854f9ad99bcf96179a94ee963
Chris Jewell <chrisjohnjewell@gmail.com>
2021-04-07 17:00:07 +1000

    Draw lines

--- app/components/canvas.ts
+++ app/components/canvas.ts
@@ -1,12 +1,16 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
-interface CanvasArgs {}
+
 // TODO
 // add a type for colour
 // add a type for a point
-// embiggen the canvas
-// use the canvas size for max random numbers
+// reuseable code cleanup
+// clear canvas button
+// store current drawing colour as state
+// implement fan mode
+// implement rainbow road
+
+interface CanvasArgs {}
 
 const Colours = [
   'green',
@@ -22,7 +26,8 @@ const Colours = [
 
 export default class Canvas extends Component<CanvasArgs> {
   canvasElement?: HTMLCanvasElement;
-  @tracked isDrawing = false;
+  drawingX: number | null = null;
+  drawingY: number | null = null;
 
   randomNumber(max: number) {
     return Math.floor(Math.random() * Math.floor(max));
@@ -38,9 +43,11 @@ export default class Canvas extends Component<CanvasArgs> {
   @action
   drawRandomRectangle() {
     const color = Colours[this.randomNumber(Colours.length)];
+    const element = this.canvasElement!;
+
     this.renderSquare(
-      this.randomNumber(100),
-      this.randomNumber(100),
+      this.randomNumber(element.width),
+      this.randomNumber(element.height),
       color,
       this.randomNumber(200)
     );
@@ -61,36 +68,51 @@ export default class Canvas extends Component<CanvasArgs> {
   @action
   drawRandomLine() {
     const colour = Colours[this.randomNumber(Colours.length)];
+    const element = this.canvasElement!;
+
     this.drawLine(
-      this.randomNumber(100),
-      this.randomNumber(100),
-      this.randomNumber(100),
-      this.randomNumber(100),
+      this.randomNumber(element.width),
+      this.randomNumber(element.height),
+      this.randomNumber(element.width),
+      this.randomNumber(element.height),
       colour
     );
   }
 
   @action
-  startDrawing() {
-    this.isDrawing = true;
+  startDrawing(mouseEvent: MouseEvent) {
+    this.drawingX = mouseEvent.clientX - (this.canvasElement?.offsetLeft || 0);
+    this.drawingY = mouseEvent.clientY - (this.canvasElement?.offsetTop || 0);
   }
 
   @action
   keepDrawing(mouseEvent: MouseEvent) {
-    if (this.isDrawing) {
-      const color = 'green';
+    if (this.drawingX && this.drawingY) {
+      const colour = Colours[this.randomNumber(Colours.length)];
 
       const x = mouseEvent.clientX - (this.canvasElement?.offsetLeft || 0);
       const y = mouseEvent.clientY - (this.canvasElement?.offsetTop || 0);
-      const size = 1;
+      this.drawLine(this.drawingX, this.drawingY, x, y, colour);
+      this.drawingX = x;
+      this.drawingY = y;
+    }
+  }
 
-      this.renderSquare(x, y, color, size);
+  @action
+  keepDrawingFan(mouseEvent: MouseEvent) {
+    if (this.drawingX && this.drawingY) {
+      const colour = Colours[this.randomNumber(Colours.length)];
+
+      const x = mouseEvent.clientX - (this.canvasElement?.offsetLeft || 0);
+      const y = mouseEvent.clientY - (this.canvasElement?.offsetTop || 0);
+      this.drawLine(this.drawingX, this.drawingY, x, y, colour);
     }
   }
 
   @action
   stopDrawing() {
-    this.isDrawing = false;
+    this.drawingX = null;
+    this.drawingY = null;
   }
 
   @action
